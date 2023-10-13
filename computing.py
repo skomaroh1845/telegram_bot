ed9c932c0c24b823d52a9b48af041c278c7d3b11
skomaroh1845 <komarov.1845@gmail.com>
2023-10-13 16:50:21 +0300

    Added dealing with list of samples

--- computing.py
+++ computing.py
@@ -1,5 +1,5 @@
 import os
-from config import scripts_paths, path_to_venv, path_to_input, path_to_output, user
+from config import scripts_paths, path_to_venv, path_to_input, path_to_output, user, NUM_OF_INSTANCE
 import pandas as pd
 
 
@@ -27,6 +27,11 @@ class Computer:
         self.job_num = ''
         self.output_text = ''
         self.running = False
+        self.r_site = ''
+        self.in_list = []
+        self.out_list = []
+        self.sample_names = []
+        self.curr_sample = 0
         if input_dir is None:
             self.input_dir = path_to_input
         else:
@@ -36,8 +41,24 @@ class Computer:
         else:
             self.output_dir = output_dir
 
+    def set_dir_lists(self, dirs):
+        for dir in dirs:
+            self.sample_names.append(dir + '_' + self.r_site.split('_')[0].split('-')[0])
+            self.in_list.append(self.input_dir + '/' + dir)
+            self.out_list.append(self.output_dir + '/' + dir + '/' + self.r_site.split('_')[0].split('-')[0])
+        print(self.in_list)
+        print(self.out_list)
+
+    def add_r_site(self, site):
+        part1 = site.split('/')[0]
+        r2_start = site.split('/')[1]
+        if self.r_site != '':
+            self.r_site = self.r_site + '_'
+        self.r_site = self.r_site + part1 + r2_start + '-' + r2_start
+        print(self.r_site)
+
     def load_from_file(self):
-        with open('job_states.txt', 'r') as f:
+        with open(f'job_states{NUM_OF_INSTANCE}.txt', 'r') as f:
             for i in self.stages:
                 if f.readline().rstrip('\n\r') == 'True':
                     self.stages[i] = True
@@ -47,28 +68,43 @@ class Computer:
             self.job_num = f.readline().rstrip('\n\r')
             self.input_dir = f.readline().rstrip('\n\r')
             self.output_dir = f.readline().rstrip('\n\r')
+            self.r_site = f.readline().rstrip('\n\r')
+            self.curr_sample = int(f.readline().rstrip('\n\r'))
+            num_of_samples = int(f.readline().rstrip('\n\r'))
+            for i in range(num_of_samples):
+                self.sample_names.append(f.readline().rstrip('\n\r'))
+                self.in_list.append(f.readline().rstrip('\n\r'))
+                self.out_list.append(f.readline().rstrip('\n\r'))
             print(self.input_dir, self.output_dir)
-            self.output_text = f'last state loaded: curr_step == {self.curr_stage}'
+            self.output_text = f'last state loaded: curr_step == {self.curr_stage} \n curr_sample == {self.sample_names[self.curr_sample]}'
 
 
     def save_to_file(self):
-        with open('job_states.txt', 'w') as f:
+        with open(f'job_states{NUM_OF_INSTANCE}.txt', 'w') as f:
             for i in self.stages:
                 f.write(str(self.stages[i]) + '\n')
             f.write(str(self.curr_stage) + '\n')
             f.write(self.job_num + '\n')
             f.write(self.input_dir + '\n')
             f.write(self.output_dir + '\n')
+            f.write(self.r_site + '\n')
+            f.write(str(self.curr_sample) + '\n')
+            f.write(str(len(self.sample_names)) + '\n')  # write num of samples
+            for i in range(len(self.sample_names)):
+                f.write(self.sample_names[i] + '\n')
+                f.write(self.in_list[i] + '\n')
+                f.write(self.out_list[i] + '\n')
+
 
 
     def start_stage(self, stage_num):
         try:
             os.system(f'ssh {user} ' +
-                    f'"sbatch start_python_16cpu.sh -e {path_to_venv} -f {scripts_paths[stage_num]} -i {self.input_dir} -o {self.output_dir}" ' +
-                      '> output.txt')
+                    f'"sbatch start_python_8cpu.sh -e {path_to_venv} -f {scripts_paths[stage_num]} -i {self.in_list[self.curr_sample]} -o {self.out_list[self.curr_sample]} -r {self.r_site}" ' +
+                      f'> output{NUM_OF_INSTANCE}.txt')
             self.curr_stage = stage_num
             try:
-                with open('output.txt', 'r') as f:
+                with open(f'output{NUM_OF_INSTANCE}.txt', 'r') as f:
                     text = f.read()
                     self.job_num = text.split(' ')[-1].strip('\n')
                     self.output_text = f'Stage {stage_num}: ' + text
@@ -83,9 +119,9 @@ class Computer:
             self.output_text = ''
             return
         try:
-            os.system(f'ssh {user} "sacct -j {self.job_num}" > output.txt')
+            os.system(f'ssh {user} "sacct -j {self.job_num}" > output{NUM_OF_INSTANCE}.txt')
             try:
-                df = pd.read_table('output.txt')
+                df = pd.read_table(f'output{NUM_OF_INSTANCE}.txt')
                 if len(df) >= 2:
                     array = str(df.iloc[1, 0]).split(' ')
                     array = [i for i in array if i]
@@ -104,9 +140,9 @@ class Computer:
         if self.job_num == '':
             return
         try:
-            os.system(f'ssh {user} "cat slurm-{self.job_num}.out" > output.txt')
+            os.system(f'ssh {user} "cat slurm-{self.job_num}.out" > output{NUM_OF_INSTANCE}.txt')
             try:
-                with open('output.txt', 'r') as f:
+                with open(f'output{NUM_OF_INSTANCE}.txt', 'r') as f:
                     text = f.read()
                     self.output_text = f'Stage {self.curr_stage} feed back:'+ '\n' + text
             except:
--- handlers.py
+++ handlers.py
@@ -8,7 +8,7 @@ from time import sleep
 
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.dispatcher.filters import Command, Text
-from config import admin_id, user
+from config import admin_id, user, path_to_input, path_to_output, NUM_OF_INSTANCE
 
 from menu import menu, common_commands
 
@@ -26,16 +26,35 @@ async def shutdowning(dp):
 
 @dp.message_handler(Text(equals=['?']))
 async def help(message: Message):
-    await message.answer(text='usage:\n' + '$[command]')
+    await message.answer(text=text_replace('commands:'))
+    await message.answer(text=text_replace('$[command_slurm]'))
+    await message.answer(text=text_replace('add_rsite='))
+    await message.answer(text=text_replace('reset='))
+    await message.answer(text=text_replace('input='))
+    await message.answer(text=text_replace('output='))
+    await message.answer(text=text_replace('stage='))
+    await message.answer(text=text_replace('set_dir_list\ndir1\ndir2\n...'))
+    await message.answer(text=text_replace('/menu'))
+    await message.answer(text=text_replace('/common'))
+
+
+@dp.message_handler(Text(startswith='set_dir_list'))
+async def set_dir_list(message: Message):
+    dirs = message.text.split('\n')
+    if len(dirs) < 2:
+        await message.answer(text=text_replace('usage:\nset_dir_list\ndir1\ndir2\n...\nwhere dir_i - name of input subfolder'))
+    else:
+        computer.set_dir_lists(dirs[1:])
+        await message.answer(text=text_replace('dirs list written'))
 
 
 @dp.message_handler(Text(startswith='$'))
 async def terminal(message: Message):
     print('Command: ' + message.text[1:] + '\n')
     try:
-        os.system(f'ssh {user} "' + message.text[1:] + '" > output.txt')
+        os.system(f'ssh {user} "' + message.text[1:] + f'" > output{NUM_OF_INSTANCE}.txt')
         try:
-            with open('output.txt', 'r') as f:
+            with open(f'output{NUM_OF_INSTANCE}.txt', 'r') as f:
                 text = f.read()
         except:
             text = 'output file cannot be read'
@@ -45,17 +64,49 @@ async def terminal(message: Message):
     text = text_replace(text)
     await message.answer(text=text)
 
+@dp.message_handler(Text(startswith="add_rsite="))
+async def set_rsite(message: Message):
+    site = ''
+    try:
+        site = message.text[10:]
+    except:
+        pass
+    if site != '':
+        computer.add_r_site(message.text[10:])
+        computer.save_to_file()
+        await message.answer(text=text_replace(f'added restrict site: {message.text[10:]}'))
+    else:
+        await message.answer(text=text_replace('usage:\n' + 'add_rsite=NN/NN'))
 
-@dp.message_handler(Text(startswith="/input: "))
+@dp.message_handler(Text(startswith="reset="))
+async def reset_parameters(message: Message):
+    param = ''
+    try:
+        param = message.text[6:]
+    except:
+        pass
+    if param == 'input':
+        computer.input_dir = path_to_input
+    elif param == 'output':
+        computer.output_dir = path_to_output
+    elif param == 'rsites':
+        computer.r_site = ''
+    else:
+        await message.answer(text=text_replace(f'parameters:\ninput\noutput\nstage\nrsites'))
+        return
+    await message.answer(text=text_replace(f'parameter {param} reset to default'))
+
+
+@dp.message_handler(Text(startswith="input="))
 async def set_input(message: Message):
-    computer.input_dir = message.text[8:]
+    computer.input_dir = message.text[6:]
     computer.save_to_file()
     await message.answer(text=text_replace(f'input dir changed to: {computer.input_dir}'))
 
 
-@dp.message_handler(Text(startswith="/output: "))
+@dp.message_handler(Text(startswith="output="))
 async def set_output(message: Message):
-    computer.output_dir = message.text[9:]
+    computer.output_dir = message.text[7:]
     computer.save_to_file()
     await message.answer(text=text_replace(f'output dir changed to: {computer.output_dir}'))
 
@@ -74,9 +125,9 @@ async def actions(message: Message):
         command = 'ls -1rt | tail -n1'
     print('Command: ' + command + '\n')
     try:
-        os.system(f'ssh {user} "' + command + '" > output.txt')
+        os.system(f'ssh {user} "' + command + f'" > output{NUM_OF_INSTANCE}.txt')
         try:
-            with open('output.txt', 'r') as f:
+            with open(f'output{NUM_OF_INSTANCE}.txt', 'r') as f:
                 text = f.read()
         except:
             text = 'output file cannot be read'
@@ -88,9 +139,18 @@ async def actions(message: Message):
     await message.answer(text=text)
 
 
-@dp.message_handler(Text(startswith="/stage: "))
+@dp.message_handler(Text(startswith="sample="))
+async def set_sample(message: Message):
+    computer.curr_sample = int(message.text[7:])
+    computer.job_num = ''
+    print(f'curr sample changed to {computer.curr_sample}')
+    computer.save_to_file()
+    await message.answer(text=text_replace(f'curr sample changed to {computer.curr_sample}'))
+
+
+@dp.message_handler(Text(startswith="stage="))
 async def set_stage(message: Message):
-    computer.curr_stage = int(message.text[8:])
+    computer.curr_stage = int(message.text[6:])
     for i in computer.stages:
         if i < computer.curr_stage:
             computer.stages[i] = True
@@ -184,9 +244,23 @@ async def checking():
             break
         if computer.stages[computer.curr_stage] is True:
             if computer.curr_stage == 15:
-                await bot.send_message(chat_id=admin_id, text=text_replace(f'Calculation completed. \nOverall duration: {all} minutes.'))
-                break
-            computer.curr_stage += 1
+                await bot.send_message(chat_id=admin_id, text=text_replace(f'Calculation completed. Sample {computer.sample_names[computer.curr_sample]} \nOverall duration: {all} minutes.'))
+                if len(computer.sample_names) - computer.curr_sample > 1:
+                    computer.curr_sample += 1
+                    all = 0
+                    computer.curr_stage = 1
+                    for i in computer.stages:
+                        if i < computer.curr_stage:
+                            computer.stages[i] = True
+                        else:
+                            computer.stages[i] = False
+                    computer.job_num = ''
+                    computer.save_to_file()
+                    minutes = 0
+                else:
+                    break
+            else:
+                computer.curr_stage += 1
             computer.start_stage(computer.curr_stage)
             print("checkup: " + computer.output_text)
             text = text_replace(computer.output_text + f'\nPrevious stage duration: {minutes} minutes.')
